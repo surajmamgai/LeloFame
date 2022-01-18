ad4baa1f1cc97474b22667898a0e83b4ae5848d5
surajmamgai <suraj365311@gmail.com>
2022-01-18 12:40:17 +0530

    dashboard

--- LeloFame/FYIT/views.py
+++ LeloFame/FYIT/views.py
@@ -3,4 +3,8 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request,"index.html")
+
+def dashboard(request):
+    return render(request,"dashboard.html")
+    
     
