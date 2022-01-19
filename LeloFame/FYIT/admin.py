2df957edf8245e30db65182117dd345dfbe72bd6
Swastik8750 <Swastik8750@gmail.com>
2022-01-19 11:41:16 +0530

    login signup error resolved

--- LeloFame/FYIT/admin.py
+++ LeloFame/FYIT/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-
 # Register your models here.
+from .models import *
 
-from .models import Profile
-admin.site.register(Profile)
+
+class ProfileLog(admin.ModelAdmin):
+    list_display = [f.name for f in Profile._meta.fields]
+    search_fields = ('username','name','first_name', 'last_name', 'email', 'credits')
+admin.site.register(Profile,ProfileLog)
+admin.site.register(CreditLog)
+admin.site.register(CreditPurchaseLog)
+admin.site.register(LeloFameRequest)
