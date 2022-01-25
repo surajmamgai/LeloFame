49b1197e41ac836646fe8ba25c5da3c260070504
Swastik8750 <Swastik8750@gmail.com>
2022-01-25 11:37:22 +0530

    utils for credit purchase and lelofame request

--- LeloFame/FYIT/admin.py
+++ LeloFame/FYIT/admin.py
@@ -6,7 +6,7 @@ class ProfileLog(admin.ModelAdmin):
     list_display = [f.name for f in Profile._meta.fields]
     search_fields = ('username','name','first_name', 'last_name', 'email', 'credits')
 admin.site.register(Profile,ProfileLog)
-admin.site.register(CreditLog)
+admin.site.register(LeloFameLog)
 admin.site.register(CreditPurchaseLog)
 admin.site.register(LeloFameRequest)
 admin.site.register(CreditPurchaseRequest)
