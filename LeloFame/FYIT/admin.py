4db3b653a89acea6470a68605f6abc3f3b1de271
Swastik8750 <Swastik8750@gmail.com>
2022-01-29 13:32:04 +0530

    referral system

--- LeloFame/FYIT/admin.py
+++ LeloFame/FYIT/admin.py
@@ -3,10 +3,11 @@ from django.contrib import admin
 from .models import *
 
 class ProfileLog(admin.ModelAdmin):
-    list_display = [f.name for f in Profile._meta.fields]
+    list_display = ['username','name','first_name', 'last_name', 'reffered_by', 'email', 'credits']
     search_fields = ('username','name','first_name', 'last_name', 'email', 'credits')
 admin.site.register(Profile,ProfileLog)
 admin.site.register(LeloFameLog)
 admin.site.register(CreditPurchaseLog)
 admin.site.register(LeloFameRequest)
 admin.site.register(CreditPurchaseRequest)
+admin.site.register(Referral)
